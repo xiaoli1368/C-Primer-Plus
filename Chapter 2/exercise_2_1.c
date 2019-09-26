66a43d36e2e598f5845def8d18a05c1c78134efc
xiaoli <1004285399@qq.com>
2019-09-26 22:52:24 +0800

    add chapter 2 some exercises

---
+++ Chapter 2/exercise_2_1.c
@@ -0,0 +1,20 @@
+/*****************************************
+ * CopyRight @ 2019-2029 
+ * Func: print name
+ * Auth: xiaoli
+ * Date: 2019/09/26
+ ****************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv)
+{
+	printf("Gustav Mahler\n");
+	printf("Gustav\nMahler\n");
+	printf("Gustav ");
+	printf("Mahler\n");
+
+	system("pause");
+	return 0;
+}
