eaedf5f23c704ca361100361d052537842b4bef4
xiaoli <xiaoli1644@qq.com>
2019-09-26 22:52:24 +0800

    add chapter 2 some exercises

---
+++ Chapter 2/exercise_2_2.c
@@ -0,0 +1,18 @@
+/*****************************************
+ * CopyRight @ 2019-2029 
+ * Func: print name and address
+ * Auth: xiaoli
+ * Date: 2019/09/26
+ ****************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv)
+{
+	printf("My name is : [xiaoli].\n");
+	printf("My address is: [Beijing].\n");
+
+    system("pause");
+	return 0;
+}
